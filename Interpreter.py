88d84e695d346edd70d3ba7c5251908389e2bf5a
emjvp <emjvp37@gmail.com>
2020-04-26 22:53:58 -0500

    clases

--- Interpreter.py
+++ Interpreter.py
@@ -5,51 +5,92 @@ import copy
 
 """
 Language restrictions
-    integers restrictions
+    restrictions
     n > 0, m > 0
 
     language restrictions
     a^n, b^m, c⁽2n+m⁾
-"""     
+"""   
+class States():
+    def __init__(self):        
+        self.InitialSt = 1
+        self.SecondSt = 2
+        self.ThirdSt = 3
+    def getInitialSt(self):
+        return self.InitialSt
+    def getSecondSt(self):
+        return self.SecondSt
+    def getThirdSt(self):
+        return self.ThirdSt
 
-#S = { UTH }  
-#States or production rules
-#P = {
-#(1) S //simbolo inicial
-U = ''    
-T = ''
-H = ''
-#(2) 
-a = 'a' #simbolo terminal
-#(3)
-U2 = a+U
-#(4)
-b = 'b' #simbolo terminal
-#(5)
-T2 = b+T
-#(6)
-c = 'c' #simbolo terminal
-#(7)
-H2 = c+H
-#}
+States1 = States()
 
-string = 'abccc' #input
-string_c = copy.copy(string)
-string_f = ''
-def is_part_of():    
-    pass
+class Interpreter():
+    def __init__(self):        
+        self.Fstring = ''
+        self.S = States1.InitialSt        
+    def getFstring(self):
+        return self.Fstring
+    def St_Base(self, string):
+        if string[0] == string[1] and len(string) >= 5:
+            self.Fstring += string[0] + string[1]
+            string1 = copy.copy(string)    
+            return string1[2:]        
+        else:
+            return string1 
+                            
+    def St_Initial(self, string):
+        if string[0] == 'a':
+            string1 = self.St_Base(string)            
+            return string1[1:]
 
- 
+    def St_Second(self, string):
+        if string[0] == 'b':
+            string1 = self.St_Base(string)
+            self.S = States1.getSecondSt()
+            return string1[1:]
+
+    def St_Third(self, string):
+        if string[0] == 'c':
+            string1 = self.St_Base(string)
+            self.S = States1.getThirdSt()
+            return string1
+        
+class Evaluator():
+    def __init__(self):
+        self.Interpreter1 = Interpreter()
+    def Is_part_of_L(self):
+        string = ''
+        string1 = ''        
+        string2 = ''
+        string3 = ''
+        
+        print("Ingrese la Cadena a evaluar")
+        string = input()
+
+        while True:
+            if self.Interpreter1.St_Base(string) == string or self.Interpreter1.getFstring() == string:
+                break
+            else:
+                string1 = self.Interpreter1.St_Initial(string)
+                print("string1 "+str(string1))
+                string2 = self.Interpreter1.St_Second(string1)
+                print("string2 "+str(string2))
+                string3 = self.Interpreter1.St_Third(string2)
+                print("string3 "+str(string3))
 
 
 
+Evaluator1 = Evaluator()
+Evaluator1.Is_part_of_L()
 
-
-
+            
 
 
 
     
-        
-        
+
+            
+
+
     
