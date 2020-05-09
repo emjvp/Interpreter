b5ae273ac16e6f622b63a410783df74eebbc896d
= <=>
2020-05-08 22:24:37 -0500

    faltan pruebas

--- Interpreter.py
+++ Interpreter.py
@@ -11,79 +11,116 @@ Language restrictions
     language restrictions
     a^n, b^m, c⁽2n+m⁾
 """   
-class States():
-    def __init__(self):        
-        self.InitialSt = 1
-        self.SecondSt = 2
-        self.ThirdSt = 3
-    def getInitialSt(self):
-        return self.InitialSt
-    def getSecondSt(self):
-        return self.SecondSt
-    def getThirdSt(self):
-        return self.ThirdSt
-
-States1 = States()
 
 class Interpreter():
     def __init__(self):        
-        self.Fstring = ''
-        self.S = States1.InitialSt        
-    def getFstring(self):
-        return self.Fstring
-    def St_Base(self, string):
-        if string[0] == string[1] and len(string) >= 5:
-            self.Fstring += string[0] + string[1]
-            string1 = copy.copy(string)    
-            return string1[2:]        
-        else:
-            return string1 
-                            
-    def St_Initial(self, string):
-        if string[0] == 'a':
-            string1 = self.St_Base(string)            
-            return string1[1:]
+        a_count = int()
+        b_count = int()
 
-    def St_Second(self, string):
-        if string[0] == 'b':
-            string1 = self.St_Base(string)
-            self.S = States1.getSecondSt()
-            return string1[1:]
+        curr_state = int() #current state
+        stateI = 1 #state UTH
+        state2 = 2 #state U->a
+        state3 = 3 #state U->aU
+        state4 = 4 #state T->b
+        state5 = 5 #state T->bT
+        state6 = 6 #final state H->c
+        state7 = 7 #final state H->cH
 
-    def St_Third(self, string):
-        if string[0] == 'c':
-            string1 = self.St_Base(string)
-            self.S = States1.getThirdSt()
-            return string1
-        
-class Evaluator():
-    def __init__(self):
-        self.Interpreter1 = Interpreter()
-    def Is_part_of_L(self):
-        string = ''
-        string1 = ''        
-        string2 = ''
-        string3 = ''
-        
-        print("Ingrese la Cadena a evaluar")
+    def isPartOf(self):
+
+        string_copied = str()
+        curr_char = str()
+        curr_state = self.stateI
+
+        print("Enter a valid string")
         string = input()
 
-        while True:
-            if self.Interpreter1.St_Base(string) == string or self.Interpreter1.getFstring() == string:
-                break
+        string_copied = copy.copy(string)
+
+        while len(string_copied) > 0:
+
+            curr_char = string_copied[0]
+            if curr_char == 'a' and curr_char == string_copied[1]:                
+                self.a_count += 1
+                string_copied[1:]
+                self.curr_state = self.state3
+            elif curr_char == 'a' and curr_char != string_copied[1]:
+                self.a_count += 1                
+                string_copied[1:]
+                self.curr_state = self.state2                
+            elif curr_char == 'b' and curr_char == string_copied[1]:                                 
+                self.b_count += 1                
+                string_copied[1:]
+                self.curr_state = self.state4
+            elif curr_char == 'b' and curr_char != string_copied[1]:
+                self.b_count += 1
+                string_copied[1:]
+                self.curr_state = self.state5
+            elif curr_char == 'c' and curr_char == string_copied[1]:                                 
+                self.c_count += 1                
+                string_copied[1:]
+                self.curr_state = self.state6
+            elif curr_char == 'c' and curr_char != string_copied[1]:
+                self.c_count += 1
+                string_copied[1:]
+                self.curr_state = self.state7            
+
+        if (self.curr_state == self.state6 or self.curr_state == self.state7) and self.c_count == (2*self.a_count) + self.b_count:
+            return str(string)+" is part of the languaje"
+        else:
+            return str(string)+" is not part of the languaje"
+
+    def generateString(self):
+        n = int()
+        m = int()
+        n_copy = int()
+        m_copy = int()
+        c_count = int()
+        f_string = str()
+        print("Enter an integer value for n")
+        n = int(input())
+        print("Enter an integer value for m")
+        m = int(input())
+        n_copy = n
+        m_copy = m
+        c_count = (2*n)+m
+        while n_copy > 0 or m_copy > 0 or c_count:
+            if n_copy > 0:
+                self.curr_state = self.state3
+                f_string += 'a'
+                n_copy -= 1
             else:
-                string1 = self.Interpreter1.St_Initial(string)
-                print("string1 "+str(string1))
-                string2 = self.Interpreter1.St_Second(string1)
-                print("string2 "+str(string2))
-                string3 = self.Interpreter1.St_Third(string2)
-                print("string3 "+str(string3))
+                self.curr_state = self.state2
+
+            if m_copy > 0:
+                f_string += 'b'
+                m_copy -= 1
+                self.curr_state = self.state5
+            else:
+                self.curr_state = self.state4
+
+            if c_count > 0:
+                f_string += 'c'
+                self.curr_state = self.state7
+                c_count -= 1
+            else:
+                 self.curr_state = self.state6
 
 
 
-Evaluator1 = Evaluator()
-Evaluator1.Is_part_of_L()
 
+
+
+        
+
+        
+
+
+        
+
+
+
+    
             
 
 
